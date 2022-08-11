33046ef0a5009c3e07e89140e3ba5fff72bfcb43
Tom Padmanathan <80556999+BabyM0le@users.noreply.github.com>
2022-08-11 03:55:23 +0100

    Updated Code
    Cleaned up code - changed simple variables to arrays and added for loop for checking the checkboxes.

--- main.js
+++ main.js
@@ -1,51 +1,43 @@
+// Main Generate Password Function
 function passwordGenerate(){
-    var symbols = '!@#$%^&*()~_-+={}[]:;<>,.?/|'
-    var numbers = '1234567890'
-    var letters  = 'abcdefghijklmnopqrstuvwxyz'
-    var lettersCaps = letters.toUpperCase()
-    var combined = ''
-
-    var symbolsChecked = document.getElementById("check1");  
-    var numbersChecked = document.getElementById("check2");  
-    var lowercaseChecked = document.getElementById("check3");  
-    var uppercaseChecked = document.getElementById("check4");
-
-    if(symbolsChecked.checked === true){
-        combined = combined + symbols
-    }
-
-    if(numbersChecked.checked === true){
-        combined = combined + numbers
-    }
-
-    if(lowercaseChecked.checked === true){
-        combined = combined + letters
-    }
-
-    if(uppercaseChecked.checked === true){
-        combined = combined + lettersCaps
-    }
-
+    let combined = ''
     let length = document.getElementById("length").value
-    let repeated = combined.repeat(32)
-    let shuffled = repeated.split('').sort(function(){return 0.5-Math.random()}).join('')
-    let password = shuffled.slice(0, length)
+    let passwordItems = [
+        '!@#$%^&*()~_-+={}[]:;<>,.?/|',
+        '1234567890',
+        'abcdefghijklmnopqrstuvwxyz',
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    ]
+    let passwordAltercations = [
+        document.getElementById("check1"),
+        document.getElementById("check2"),  
+        document.getElementById("check3"),  
+        document.getElementById("check4"),
+    ]
+    for(let i = 0; i < 4; i++) {
+        if(passwordAltercations[i].checked === true){
+            combined = combined + passwordItems[i]
+        }
+    }
+    let password = combined.repeat(32).split('').sort(function(){return 0.5-Math.random()}).join('').slice(0, length)
 
     if(length < 4){
         document.getElementById("passwordError").innerHTML='Password too short (4 - 32)'
     } else if(length > 32){
         document.getElementById("passwordError").innerHTML='Password too long (4 - 32)'
-    } else {
+    } else{
         document.getElementById("passwordOutput").innerHTML=password
         document.getElementById("passwordError").innerHTML=''
     }
 }
 
+// Copy Password Function
 function copyPassword(){
     let copyPassword = document.getElementById("passwordOutput").value
     navigator.clipboard.writeText(copyPassword)
 }
 
+// Clear Password Function
 function clearPassword(){
     document.getElementById("passwordOutput").innerHTML=''
 }
