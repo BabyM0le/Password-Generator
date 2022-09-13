df6441d9b4e9a6ea0e675b8fe1bd17db0284bdf9
Tom Padmanathan <padmanathantom@gmail.com>
2022-09-13 15:28:10 +0100

    Created error function, made all quotations marks uniform and added more comments

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Main Generate Password Function
 function passwordGenerate(){
     let combined = ''
-    const length = document.getElementById("length").value
+    const length = document.getElementById('length').value
     const passwordItems = [
         '!@#$%^&*()~_-+={}[]:;<>,.?/|',
         '1234567890',
@@ -9,37 +9,47 @@ function passwordGenerate(){
         'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
     ]
     const passwordAltercations = [
-        document.getElementById("check1"),
-        document.getElementById("check2"),  
-        document.getElementById("check3"),  
-        document.getElementById("check4"),
+        document.getElementById('check1'),
+        document.getElementById('check2'),  
+        document.getElementById('check3'),  
+        document.getElementById('check4'),
     ]
+
+    // Adds Current Checkboxes
     for(let i = 0; i < 4; i++) {
         if(passwordAltercations[i].checked == true){
             combined += passwordItems[i]
         }
     }
+
+    // Generates Password
     const password = combined.repeat(32).split('').sort(
         function() {
             return 0.5-Math.random()
         }).join('').slice(0, length)
 
+    // Call Errors
     if(length < 4){
-        document.getElementById("passwordError").innerHTML='Password too short (4 - 32)'
+        error('Password too short (4 - 32)')
     } else if(length > 32){
-        document.getElementById("passwordError").innerHTML='Password too long (4 - 32)'
+        error('Password too long (4 - 32)')
     } else{
-        document.getElementById("passwordOutput").innerHTML=password
-        document.getElementById("passwordError").innerHTML=''
+        document.getElementById('passwordOutput').innerHTML=password
+        error('')
     }
 }
 
 // Copy Password Function
 function copyPassword(){
-    navigator.clipboard.writeText(document.getElementById("passwordOutput").value)
+    navigator.clipboard.writeText(document.getElementById('passwordOutput').value)
 }
 
 // Clear Password Function
 function clearPassword(){
-    document.getElementById("passwordOutput").innerHTML=''
+    document.getElementById('passwordOutput').innerHTML=''
+}
+
+// Error
+function error(error){
+    document.getElementById('passwordError').innerHTML=error
 }
