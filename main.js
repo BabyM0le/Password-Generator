578b952515740cb31afe8ebf6068f4bcdec8dfc5
Tom Padmanathan <padmanathantom@gmail.com>
2022-09-20 15:05:22 +0100

    Cleaned up code

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-// Main generate password function
-function passwordGenerate(){
-    let combined = ''
-    const length = document.getElementById('length').value
-    const passwordItems = [
+const passwordItems = [
         '!@#$%^&*()~_-+={}[]:;<>,.?/|',
         '1234567890',
         'abcdefghijklmnopqrstuvwxyz',
         'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-    ]
+]
+
+// Main generate password function
+function passwordGenerate(){
+    let password = ''
+    const length = document.getElementById('length').value
     const passwordAltercations = [
         document.getElementById('check1'),
         document.getElementById('check2'),  
@@ -16,40 +17,31 @@ function passwordGenerate(){
     ]
 
     // Adds active checkboxes to a combined total
-    for(let i = 0; i < 4; i++) {
-        if(passwordAltercations[i].checked == true){
-            combined += passwordItems[i]
-        }
-    }
+    for(let i = 0; i < 4; i++) if(passwordAltercations[i].checked == true) password += passwordItems[i]
 
-    // Generates assword
-    const password = combined.repeat(32).split('').sort(
-        function() {
-            return 0.5-Math.random()
-        }).join('').slice(0, length)
+    // Generates password
+    password = password.repeat(32).split('').sort(function() { return 0.5-Math.random() }).join('').slice(0, length)
 
     // Call errors
-    if(length < 4){
-        error('Password too short (4 - 32)')
-    } else if(length > 32){
-        error('Password too long (4 - 32)')
-    } else{
+    if(length < 4) error('Password too short (4 - 32)')
+    else if(length > 32) error('Password too long (4 - 32)')
+    else {
         document.getElementById('passwordOutput').innerHTML=password
         error('')
     }
 }
 
 // Copy password function
-function copyPassword(){
+function copyPassword() {
     navigator.clipboard.writeText(document.getElementById('passwordOutput').value)
 }
 
 // Clear password function
-function clearPassword(){
+function clearPassword() {
     document.getElementById('passwordOutput').innerHTML=''
 }
 
 // Error function
-function error(error){
+function error(error) {
     document.getElementById('passwordError').innerHTML=error
 }
